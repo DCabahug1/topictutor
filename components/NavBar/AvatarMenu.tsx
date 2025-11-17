9f3723f229c82f7bec370424126c631565834c62
DCabahug1 <150985638+DCabahug1@users.noreply.github.com>
2025-11-17 15:37:36 -0800

    Update AvatarMenu.tsx

--- components/NavBar/AvatarMenu.tsx
+++ components/NavBar/AvatarMenu.tsx
@@ -26,7 +26,7 @@ function AvatarMenu({ profile }: { profile: Profile }) {
     <DropdownMenu>
       <DropdownMenuTrigger className="rounded-full border-2 border-primary/70 hover:border-primary transition-all duration-100 cursor-pointer">
         <Avatar>
-          <AvatarImage src={profile.image_url} />
+          <AvatarImage src={profile.image_url} className="object-cover" />
           <AvatarFallback className="text-primary-foreground bg-primary">
             {profile.name?.[0]?.toUpperCase() ||
               profile.email?.[0].toUpperCase()}
